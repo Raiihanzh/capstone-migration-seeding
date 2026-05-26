3009aa579313fb385a4d3ea0cd612e88550ace2d
Raihanzh <zulhilmiraihan92@gmail.com>
2026-05-26 23:51:11 +0700

    add read validation feature

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -5,24 +5,35 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Models\UserModel; 
+
 class AuthController extends BaseController
 {
+    protected $userModel;
+
    function __construct()
     {
         helper('form');
+        $this->userModel = new UserModel();
     }
 
 public function login()
     {
     if ($this->request->getPost()) {
-        $username = $this->request->getVar('username');
-        $password = $this->request->getVar('password');
+    $rules = [
+    'username' => 'required|min_length[6]',
+    'password' => 'required|min_length[7]|numeric',
+    ];
 
-        $dataUser = ['username' => 'Raihan', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin', 'email' => 'raihan@gmail.com']; // passw 123
+    if ($this->validate($rules)){
+    $username = $this->request->getPost('username');
+    $password = $this->request->getPost('password');
 
-        if ($username == $dataUser['username']) {
-            if (md5($password) == $dataUser['password']) {
-session()->set([
+    $dataUser = $this->userModel ->where(['username' => $username])->first();
+
+        if ($dataUser) {
+	      if (password_verify($password, $dataUser['password'])) {
+    session()->set([
     'username'   => $dataUser['username'],
     'email'      => 'raihan@gmail.com',
     'role'       => $dataUser['role'],
@@ -39,6 +50,11 @@ session()->set([
             session()->setFlashdata('failed', 'Username Tidak Ditemukan');
             return redirect()->back();
         }
+    } else {
+    session()->setFlashdata('failed', $this->validator->listErrors());
+    return redirect()->back();
+}
+    
     } else {
         return view('v_login');
     }
